ebc200c17b931ff1a8eff617b29e1ce277f4f154
freeyungcoop <50121590+freeyungcoop@users.noreply.github.com>
2020-03-04 14:53:48 -0600

    Initial commit

---
+++ forms/animals/animals.js
@@ -0,0 +1,9 @@
+/*
+let animals = ["dog","cat","horse","mouse","pig","cow","ferret","lizard","frog"]
+
+let enter = prompt("Enter the name of an animal to add it to the array")
+let newAnimal = enter.toLowerCase()
+
+  animals.push(newAnimal)
+  alert(`The last animal is ${animals[animals.length - 1]}`)
+*/
---
+++ forms/calculate/calculate.js
@@ -0,0 +1,13 @@
+/*
+function calcAvgSquare (a,b,c){
+    let avgSquare = ((a + b + c)/3) * (a**2)
+    return avgSquare
+}
+
+let num1 = Number(prompt("Enter a number"))
+let num2 = Number(prompt("Enter a second number"))
+let num3 = Number(prompt("Enter a third number"))
+
+let answer = calcAvgSquare(num1,num2,num3)
+alert(`The calculated average squared is ${answer}`)
+*/
